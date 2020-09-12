2cdc8f4f9031d1718ed88fa6e2bc661df6fded36
chethan-m <chethanmkumar19@gmail.com>
2020-09-12 08:00:49 +0530

    added a line

--- login.java
+++ login.java
@@ -3,7 +3,8 @@ public class
 	public static void main strings[] args
 	{
 		system.out.println("Hello World");
-                system.out.println("This is my first program")
+                system.out.println("This is my first program");
+	        system.out.println("Hi");
 
 	}
 }
