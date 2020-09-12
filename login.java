e023361dc9e09beb9268e5c5f9ccda4354ce27e8
chethan-m <chethanmkumar19@gmail.com>
2020-09-12 09:33:51 +0530

    added Hai

--- login.java
+++ login.java
@@ -2,9 +2,11 @@ public class
 {
 	public static void main strings[] args
 	{
+		system.out.println("Hai");
 		system.out.println("Hello World");
                 system.out.println("This is my first program");
 	        system.out.println("am learning programming");
+
 
 	}
 }
