4d4dc2c552bf174778bd7c14c3e7eeda713aab21
chethan-m <chethanmkumar19@gmail.com>
2020-09-29 23:56:03 +0530

    I am in master and I need length as 30

--- login.java
+++ login.java
@@ -1,11 +1,12 @@
 public class
 {
 	int i;
+
 	public static void main (strings args[])
 	{
           
 
-	  for(i=0;i<=10;i++)
+	  for(i=0;i<=30;i++)
           {
 
            system.out.println(" number's :"+i);
